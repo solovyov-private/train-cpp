3cb668bf5aafb77cbe60c2e100a89c2fcf7c612c
solovyov-private <nightingale@aura-hub.com>
2020-11-21 15:53:34 +0000

    add 2

--- Accelerated_CPP/other/frames.cpp
+++ Accelerated_CPP/other/frames.cpp
@@ -17,6 +17,52 @@ string::size_type width(const vector<string>& v)
     return maxlen;
 }
 
+vector<string> hcat(const vector<string>& left,
+                    const vector<string>& right)
+{
+    vector<string> ret;
+    string::size_type width1 = width(left) + 1;
+
+    vector<string>::size_type i = 0, j = 0;
+
+    while (i != left.size() || j != right.size()) {
+        string s;
+
+        if (i != left.size()){
+            s = left[i++];
+        }
+
+        s += string(width1 - s.size(), ' ');
+
+        if (j != right.size()) {
+            s += right[j++];
+        }
+
+        ret.push_back(s);
+    }
+
+    return ret;
+}
+
+vector<string> vcat(const vector<string>& top,
+                    const vector<string>& bottom)
+{
+    vector<string> ret = top;
+
+    for (
+            vector<string>::const_iterator it = bottom.begin();
+            it != bottom.end();
+            ++it
+    ) {
+        ret.push_back(*it);
+    }
+
+    // or the same
+    // ret.insert(ret.end(), bottom.begin(), bottom.end());
+
+    return ret;
+}
+
 vector<string> frame(const vector<string>& v)
 {
     vector<string> ret;
