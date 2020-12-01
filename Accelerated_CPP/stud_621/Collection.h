0ccb77b3f7382d58123b1df895eed823f00edd42
solovyov-private <solovyov.private@gmail.com>
2020-12-01 21:48:24 +0000

    upd

---
+++ Accelerated_CPP/stud_621/Collection.h
@@ -0,0 +1,8 @@
+#include <list>
+#include <vector>
+
+#include "Student_info.h"
+
+//typedef std::list<Student_info> Collection;
+typedef std::vector<Student_info> Collection;
+Collection extract_fails(Collection& students);
