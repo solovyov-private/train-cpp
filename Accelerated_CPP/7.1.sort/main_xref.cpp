d8041e79201fcf955522f09b8cefe88b0ffc45f4
solovyov-private <solovyov.private@gmail.com>
2020-12-17 16:49:47 +0000

    7.1

---
+++ Accelerated_CPP/7.1.sort/main_xref.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+#include "xref.h"
+ 
+using std::cin;
+using std::cout;
+using std::endl;
+using std::map;
+using std::string;
+using std::vector;
+
+int main()
+{
+    map<string, vector<int> > words = xref(cin);
+
+    Collection ret = sortWords(words);
+ 
+    for (Collection::const_iterator iter = ret.begin();
+       iter != ret.end(); ++iter) {
+ 
+        cout << iter->first << " occurs on line(s): ";
+     
+        vector<int>::const_iterator line_no = iter->second.begin();
+        cout << *line_no;
+        ++line_no;
+
+        while (line_no != iter->second.end()) {
+          cout << ", " << *line_no;
+          ++line_no;
+        }
+        cout << endl;
+  }
+ 
+  return 0;
+}
