cac2df9d7bffd876e45c53852ad188a279272380
solovyov-private <solovyov.private@gmail.com>
2021-03-24 12:35:54 +0000

    Added operators to Str class

--- Accelerated_CPP/12.0.Str/Str.cpp
+++ Accelerated_CPP/12.0.Str/Str.cpp
@@ -1,11 +1,20 @@
 #include <algorithm>
 #include <cstring>
+#include <iostream>
 #include "Vec.h"
 
 class Str
 {
+    friend std::istream& operator>>(std::istream&, Str&);
 public:
+    Str& operator+=(const Str& s) {
+        std::copy(s.data.begin(), s.data.end(),
+                  std::back_inserter(data));
+        return *this;
+    }
+
     typedef Vec<char>::size_type size_type;
+
     Str() { }
 
     Str(size_type n, char c): data(n, c) { }
@@ -20,11 +29,55 @@ public:
         std::copy(b, e, std::back_inserter(data));
     }
 
+    char& operator[](size_type i) { return data[i]; }
+    const char& operator[](size_type i) const { return data[i]; }
+
+    size_type size() const { return data.size(); }
+
 private:
     Vec<char> data;
 };
 
+std::istream& operator>>(std::istream&, Str&);
+std::ostream& operator<<(std::ostream&, const Str&);
+Str operator+(const Str&, const Str&);
+
+std::istream& operator>>(std::istream& is, Str& s)
+{
+    s.data.clear();
+
+    char c;
+    while (is.get(c) && isspace(c))
+        ;
+
+    if (is) {
+        do s.data.push_back(c);
+        while (is.get(c) && ! isspace(c));
+
+        if (is)
+            is.unget();
+    }
+    return is;
+}
+
+std::ostream& operator<<(std::ostream& os, const Str& s)
+{
+    for (Str::size_type i = 0; i != s.size(); ++i)
+        os << s[i];
+    return os;
+}
+
+Str operator+(const Str& s, const Str& t){
+    Str r = s;
+    r += t;
+    return r;
+}
+
 int main()
 {
     Str x;
+    std::cin >> x;
+    x += "xyi";
+    std::cout << x[1] << std::endl;
+    std::cout << x << std::endl;
 }
