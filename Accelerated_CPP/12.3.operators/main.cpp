d0833a4254f582a660259333384e783dd7ffe433
solovyov-private <solovyov.private@icloud.com>
2021-04-01 22:58:54 +0300

    12.3

---
+++ Accelerated_CPP/12.3.operators/main.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+
+#include "Str.h"
+
+int main()
+{
+    Str x("Hello");
+    std::cout << x.c_str() << std::endl;
+
+    Str y = " World ";
+    const size_t noSymbolsToCopy = 3;
+    char f[noSymbolsToCopy];
+    size_t f_size = y.copy(f, noSymbolsToCopy);
+    std::cout << f << std::endl;
+
+    std::cout << (y < x) << std::endl;
+}
